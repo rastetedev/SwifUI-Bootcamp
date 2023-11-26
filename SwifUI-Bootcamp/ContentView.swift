1a5be9c33cd279a1487de9a76531ad06e4a8ea3b
Raul Astete <rastete@acculynx.com>
2023-11-26 00:24:45 -0500

    Initial Commit

---
+++ SwifUI-Bootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwifUI-Bootcamp
+//
+//  Created by Raul Emilio Astete Acuna on 26/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwifUI-Bootcamp/SwifUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_BootcampApp.swift
+//  SwifUI-Bootcamp
+//
+//  Created by Raul Emilio Astete Acuna on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
